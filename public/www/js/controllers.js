efdc9540611a34105d8172dc5e73e239f1a56048
schenkfab <schenkfab@users.noreply.github.com>
2016-04-22 19:52:37 +0200

    feat: Autocomplete for stations

--- public/www/js/controllers.js
+++ public/www/js/controllers.js
@@ -1,8 +1,29 @@
 angular.module('starter.controllers', [])
 
-.controller('SearchCtrl', function($scope, Train){
-	console.log('Demo');
-	Train.myTrain();
+.controller('SearchCtrl', function($scope, $http){
+	$scope.from = '';
+	$scope.to = '';
+
+
+	$scope.autocompleteStation = function(query) {
+		return $http.get('http://transport.opendata.ch/v1/locations', {
+			params: {'query': query, 'type': 'station'}
+		});
+	}
+
+	$scope.search = function () {
+		console.log($scope.from);
+		console.log($scope.to);
+	}
+
+	$scope.setFromMethod = function (callback) {
+	    $scope.from = callback.selectedItems; 
+	}
+
+	$scope.setToMethod = function (callback) {
+	    $scope.to = callback.selectedItems; 
+	}
+
 })
 
 .controller('SearchDetailCtrl', function($scope, $stateParams, Train) {
